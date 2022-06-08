866d765f9bf3b220f11529eecae1b11ee00c7368
TristanDaniels <tristandaniels07@gmail.com>
2022-06-08 13:37:18 +0200

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+let sum = " ";
+function buildSum(char) {
+  sum += char;
+  document.getElementById("display").innerHTML = sum;
+}
+function calc() {
+  sum = eval(sum);
+  document.getElementById("display").innerHTML = null;
+  document.getElementById("display").innerHTML = sum;
+}
+function clrSum() {
+  document.getElementById("display").innerHTML = null;
+  sum = " ";
+}
+function changered() {
+  document.getElementById("display").style.color = "red";
+}
+function changeblue() {
+  document.getElementById("display").style.color = "blue";
+}
+function changegreen() {
+  document.getElementById("display").style.color = "green";
+}
+function changeblack() {
+  document.getElementById("display").style.color = "black";
+}
